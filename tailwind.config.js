c287abaea91a749d2c9be8685345730d1d6d096f
chididavid <davidnnonah@gmail.com>
2023-07-23 10:19:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+
+    colors: {
+         'Lightred': 'hsl(0, 100%, 67%)',
+         'Orangeyyellow': 'hsl(39, 100%, 56%)',
+         'Greenteal': 'hsl(166, 100%, 37%)',
+         'Cobaltblue': 'hsl(234, 85%, 45%)',
+         'White': 'hsl(0, 0%, 100%)',
+         'Paleblue': 'hsl(221, 100%, 96%)',
+         'Lightlavender': 'hsl(241, 100%, 89%)',
+         'Darkgrayblue': 'hsl(224, 30%, 27%)',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
